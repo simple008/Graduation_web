31309f6852a8a966c564e765442dd313369befd1
lukong <1508376369@qq.com>
2016-09-19 17:14:20 +0800

    增加上传解析方法JAR模块

--- src/main/java/com/lukong/controller/JarController.java
+++ src/main/java/com/lukong/controller/JarController.java
@@ -1,6 +1,7 @@
 package com.lukong.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,5 +14,9 @@ public class JarController {
     @RequestMapping(value = "/jar/sb",method = RequestMethod.GET)
     public String getReg(){
         return "/jar/submit";
+    }
+    @RequestMapping(value = "/jar/add/{sensor}",method = RequestMethod.GET)
+    public String getAdd(@PathVariable("sensor") String sensor){
+        return "/jar/addJar";
     }
 }
---
+++ src/main/webapp/WEB-INF/pages/jar/addJar.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lukong
+  Date: 16/9/19
+  Time: 下午5:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <h1>upload jar</h1>
+</head>
+<body>
+
+</body>
+</html>
